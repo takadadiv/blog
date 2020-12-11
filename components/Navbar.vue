b48fd031c865aa11a76305116dab018d92868d21
takadadiv <takadadiv@gmail.com>
2020-12-12 04:33:20 +0900

    bulma追加

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,19 @@
 <template lang="pug">
-  nav
-    h1 title
-    ul
-      li
-        n-link(:to="{ name: 'index' }") ブログ
+  .bulma
+    nav.navbar.is-spaced
+      .navbar-brand
+        h1.navbar-item タイトル
+        a.navbar-burger(
+          role="button"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        )
+          span(aria-hidden="true")
+          span(aria-hidden="true")
+          span(aria-hidden="true")
+      .navbar-menu
+        .navbar-end
+          n-link.navbar-item(:to="{ name: 'index' }") メモ帳
+          n-link.navbar-item(:to="{ name: 'index' }") ポートフォリオ
 </template>
-
-<style lang="stylus" scoped>
-</style>
